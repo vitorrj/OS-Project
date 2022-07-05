de72d3fa75084eb2fa5a3968a9eb1fb244a7aaf6
vitorrj <vitor.rjacinto@gmail.com>
2022-07-05 17:33:11 +0200

    Fixed theory files

--- theory/compressionUsingThreads.c
+++ theory/compressionUsingThreads.c
@@ -1,35 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <pthread.h>
 
 #define MAX 256
 
-void* compressFirstHalf(char str[]);
-void* compressSecondHalf(char str[]);
-
 char str1[MAX];
 char str2[MAX];
 
+void compression(char buffer[]);
+    void* compressFirstHalf(void* str);
+    void* compressSecondHalf(void* str);
+    
+
 int main(){
     
-    char str[MAX] = "bbbbaaaaaabbbcccccccddddeeeeeeffffffggggghhhzzzppplllqqqquuuoooxxxxmmmcccjjkkkkaaaaauuuuuwwwwwpppplllmmmnnnbbbvvvjhhhhaaaaccccpppoooaaakkjkaaaqqqqbbbccccccccrrrrrrwwwwwuuuuaaaazzzzbbbddddhhhhjjjjqqqquuueeeetttzzzzzuuubbbaaaaapppqqqqquutttttiiiiwwwwbbbbbbcc";
-
-
-    pthread_t thread1, thread2;
-
-    pthread_create(&thread1, NULL, compressFirstHalf, str);
-    pthread_create(&thread2, NULL, compressSecondHalf, str);
-
-    pthread_join(thread1, NULL);    
-    pthread_join(thread2, NULL);    
+    char buffer[MAX] = "aaaaaaaabbbbbbbbccccccddddeeeeeefffffggggghhhzzzppplllqqqquuuoooxxxxmmmcccjjkkkkaaaaauuuuuwwwwwpppplllmmmnnnbbbvvvjhhhhaaaaccccpppoooaaakkkaaaqqqqbbbccccccccrrrrrrwwwwwuuuuaaaazzzzbbbdddhhhhjjjjqqqquuueeeetttzzzzzuuubbbaaaaapppqqqqquutttttiiiiwwwwbbbbccc";
+    compression(buffer);
 
     strcat(str1, str2);
-    printf("========== La stringa da comprimere è ==========\n%s", str);
-    printf("\n\n ========== La compressione è ========== \n%s", str1);
+    printf("%s", str1);
 }
 
 
-void* compressFirstHalf(char str[]){
+// RLE COMPRESSION
+
+void compression(char buffer[]){
+
+    pthread_t thread1, thread2;
+
+    pthread_create(&thread1, NULL, compressFirstHalf, (void*)buffer);
+    pthread_create(&thread2, NULL, compressSecondHalf, (void*)buffer);
+
+    pthread_join(thread1, NULL);    
+    pthread_join(thread2, NULL);   
+
+
+
+}
+
+
+void* compressFirstHalf(void* buffer){
+
+    char *str = buffer;
 
     char currentChar = str[0];
     int count = 1;
@@ -59,7 +73,9 @@ void* compressFirstHalf(char str[]){
 
 }
 
-void* compressSecondHalf(char str[]){
+void* compressSecondHalf(void* buffer){
+
+    char *str = buffer;
 
     char currentChar = str[0];
     int count = 1;
@@ -72,7 +88,7 @@ void* compressSecondHalf(char str[]){
         else{
             str2[j] = currentChar;
             j++;
-            str2[j] = '0' + count ;
+            str2[j] = '0' + count;
             j++;
 
             currentChar = str[i];
